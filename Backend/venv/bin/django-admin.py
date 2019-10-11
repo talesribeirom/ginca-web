119ef0d2064034045b14bba8cf16b1f8e6040b89
Felipe Silva <felipe.ferreira.lemaf@gmail.com>
2019-10-11 08:57:42 -0300

    :building_construction: Adiciona o projeto Djando à pasta Backend
    Descrição: Cria a pasta backend para deixar o projeto separado

---
+++ Backend/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/labsine03/Área de Trabalho/my_folder/VSCode-linux-x64/bin/ginca-web/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
