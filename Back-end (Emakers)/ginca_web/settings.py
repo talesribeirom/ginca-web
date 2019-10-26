75bda9a1b383b8a9cebce1b6d43e005bea71d8d9
Felipe Ferreira <fe-felipe98@hotmail.com>
2019-10-26 16:34:37 -0300

    Lista participantes

--- Back-end (Emakers)/ginca_web/settings.py
+++ Back-end (Emakers)/ginca_web/settings.py
@@ -134,8 +134,8 @@ AUTH_USER_MODEL = 'users.User'
 
 # URLs referente ao login
 
-LOGIN_URL = 'login/'
+LOGIN_URL = '/login'
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/index'
 
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/login'
--- Back-end (Emakers)/users/views.py
+++ Back-end (Emakers)/users/views.py
@@ -54,5 +54,9 @@ def delete_user(request, id_user):
 
 @login_required
 def list_users(request):
-	users = User.objects.all().order_by('username')
-	return render(request, 'participants.html', {'users': users})
+    users = User.objects.all().order_by('username')
+    form = User_Creation_Form(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('url_participants')
+    return render(request, 'participants.html', {'users': users, 'form': form})
