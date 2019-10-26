15db0a5c9ebf2e941936b68a01b1c9b6effe56cf
Felipe Ferreira <33036824+focks98@users.noreply.github.com>
2019-10-26 18:53:54 -0300

    Update settings.py

--- Back-end (Emakers)/ginca_web/settings.py
+++ Back-end (Emakers)/ginca_web/settings.py
@@ -81,9 +81,9 @@ WSGI_APPLICATION = 'ginca_web.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'teste_bd',
-        'USER': 'postgres',
-        'PASSWORD': 'FEFEFE1998',
+        'NAME': 'ginca_web',
+        'USER': 'usr_gincaweb',
+        'PASSWORD': 'pwd_gincaweb',
         'HOST': 'localhost',
         'PORT': '5432',
     }
