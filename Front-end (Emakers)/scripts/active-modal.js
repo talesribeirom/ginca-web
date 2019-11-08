fd533af55a8946d9d0877ab7db11b502040a5923
jpandolpho <joao.andolpho@estudante.ufla.br>
2019-11-07 23:33:59 -0300

    :hammer: Adiciona trechos para o novo modal
    Descricao: Adiciona trechos no codigo para tratar o modal da tela de
    eventos.
    Referencia: Nenhuma.

--- Front-end (Emakers)/scripts/active-modal.js
+++ Front-end (Emakers)/scripts/active-modal.js
@@ -1,20 +1,24 @@
 /* Coded By FaBiUsKcomp - 2019 
+  modificado por jpandolpho - 2019
    Script usado para abrir modais da tela de Participantes
 */
 
 // Recebendo o Modal
 var modalUser = document.getElementById("myModalUser");
 var modalScore = document.getElementById("myModalScore");
+var modalMsg = document.getElementById("myModalMessage");
 
 // Botoes para abrir os modais
 var btnUser = document.getElementById("btn-user");
 var btnScore = document.getElementById("btn-score");
+var btnMsg = document.getElementById("btn-msg");
 
 //var body = document.getElementById("body");
 
 // Botoes para fechar os modais
 var spanUser = document.getElementsByClassName("close")[0];
 var spanScore = document.getElementsByClassName("close")[1];
+var spanMsg = document.getElementsByClassName("close")[2];
 
 // Quando o usuário clicar no botao "Adicionar Usuario"
 btnUser.onclick = function() {
@@ -25,7 +29,9 @@ btnScore.onclick = function() {
   modalScore.style.display = "block";
   //body.className += "blur-effect";
 }
-
+btnMsg.onclick = function() {
+  modalMsg.style.display = "block";
+}
 
 // Quando o usuário clicar no botao Fechar do modal "Adicionar Usuario"
 spanUser.onclick = function() {
@@ -35,6 +41,9 @@ spanUser.onclick = function() {
 spanScore.onclick = function() {
   modalScore.style.display = "none";
   //body.className -= "blur-effect";
+}
+spanMsg.onclick = function() {
+  modalMsg.style.display = "none";
 }
 
 // Quando o usuário clicar em qualquer lugar fora dos modal, serão fechado
@@ -46,5 +55,8 @@ window.onclick = function(event) {
   if (event.target == modalScore) {
     modalScore.style.display = "none";
     //body.className -= "blur-effect";
+  }
+  if (event.target == modalMsg) {
+    modalMsg.style.display = "none"
   }
 }
